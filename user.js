96f9ba931b73650b8cbb00f8413527b424d20b0b
PovSidanak <115892193+PovSidanak@users.noreply.github.com>
2023-05-31 23:50:47 +0700

    src
    router

---
+++ user.js
@@ -0,0 +1,38 @@
+const mongoose= require("mongoose");
+
+const user =mongoose.Schema({
+    username:{
+        type:String,
+        min:1,
+        max:255
+
+    },
+    email:{
+        type:String,
+        min:1,
+        max:255
+    },
+    password:{
+        type:String,
+        min:1,
+        max:255
+    },
+    firstname:{
+        type:String,
+        min:1,
+        max:255
+    },
+    lastname:{
+        type:String,
+        min:1,
+        max:255
+    },
+    created:{
+        type:Date,
+        default:Date.now()
+    }
+
+})
+
+
+module.exports=mongoosee.model("user",user)
